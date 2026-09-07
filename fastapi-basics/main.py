d006259fcd72264e8875491b62383318e879fa07
Sarthak <ncssarthak@gmail.com>
2026-09-07 09:38:38 +0530

    Add fastapi path and query parameter

---
+++ fastapi-basics/main.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+
+#     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# @app.get("/")
+# def home():
+#     return {"message": "Hello, World!"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id:int):
+#     return {"item_id": item_id}
+
+
+##Query parameter
+# @app.get("/item_name/{item_name}")
+# def read_item(item_name:str,needy:str,skip:int=0,limit=int | None ):
+#     return {"item_name":item_name,"needy":needy,"skip":skip,"limit":limit}
