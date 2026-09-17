f520c6616860a8d9ea2609ca86bddbefe9d97152
Sarthak <ncssarthak@gmail.com>
2026-09-17 10:40:37 +0530

    Add PostgreSQL database setup

---
+++ fastapi-basics/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "postgresql://postgres:Sarthak%402403@localhost/placement_api"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ fastapi-basics/init_db.py
@@ -0,0 +1,4 @@
+from database import engine, Base
+import models
+
+Base.metadata.create_all(bind=engine)
