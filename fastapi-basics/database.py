4f926a0b2f6c4ee5bba197e0b263934aa369a9a0
Sarthak <ncssarthak@gmail.com>
2026-09-22 10:36:15 +0530

    Move database config to environment variables

--- fastapi-basics/database.py
+++ fastapi-basics/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from config import settings
 
-DATABASE_URL = "postgresql://postgres:Sarthak%402403@localhost/placement_api"
+DATABASE_URL = settings.DATABASE_URL 
+
 
 engine = create_engine(DATABASE_URL)
 
